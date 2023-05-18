102e755848a3fdabd2a745899d07035f27a0f587
MD Naem <mmnayeem408@gmail.com>
2023-05-18 18:32:37 +0600

    footer section add

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -2,7 +2,7 @@
 
 const Banner = () => {
     return (
-        <div className="-mt-24 pt-36 pb-16 shadow-xl" style={{
+        <div className="-mt-60 pt-64 pb-16 shadow-xl" style={{
             backgroundImage: `url('https://c0.wallpaperflare.com/preview/265/849/245/toys.jpg')`,
             // backgroundImage: `url(${externalImage})`,
             backgroundSize: 'cover',
@@ -14,8 +14,8 @@ const Banner = () => {
                 <h1 className="font-bold text-7xl ">Shine Bright</h1>
                 <h3 className="text-4xl font-medium py-8">Like a Superstar</h3>
                 <div className=" flex gap-12 justify-center">
-                    <button className="btn btn-outline btn-secondary">Button</button>
-                    <button className="btn btn-outline btn-secondary">Button</button>
+                    <button className="btn btn-secondary">Button</button>
+                    <button className="btn btn-secondary">Button</button>
                 </div>
             </div>
         </div>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "./Banner/Banner";
+import ShopCards from "./ShopCards/ShopCards";
 
 const Home = () => {
     return (
         <div className="">
             <Banner></Banner>
+            <ShopCards></ShopCards>
             this is home
         </div>
     );
